db70b7ba4a13f62246caf4bac352a1a5903cf18c
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-10-18 21:17:33 +0300

    лнреасшгнеа

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,31 +2,57 @@ import sty from './input.module.css'
 import React, { Component } from 'react'
 import { nanoid } from 'nanoid'
 import { toBeDisabled } from '@testing-library/jest-dom/matchers'
-class TypeInp extends Component {
-	state = {
-		contacts: [
-			// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-			
-		],
-		name: '',
-		number: '',
-		filter: '',
-	}
-	addContact = (e) => {
-		const inpName = document.getElementById('inpName').value
-		const inpPhone = document.getElementById('inpPhone').value
-		let cons = {
-			id: nanoid(),
-			name: inpName,
-			number: inpPhone,
+import { useState, useEffect } from 'react'
+const TypeInp = () => {
+	const [contacts, setContacts] = useState([
+		// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+		// { id: 'id-2', name: 'Rosie Simpson', number: '459-12-56' },
+		// { id: 'id-4', name: 'Rosie Simpson', number: '459-12-56' },
+	])
+	const [filter, setFilter] = useState("")
+	const [upd, setUpd] = useState(0)
+	const [upd2, setUpd2] = useState(false)
+	const [isMounted, setIsMounted] = useState(false)
+
+	// state = {
+	// 	contacts: [
+	// 		// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+
+	// 	],
+	// 	name: '',
+	// 	number: '',
+	// 	filter: '',
+	// }
+
+	const addContact = () => {
+		if (upd2 === false) {
+			setUpd('r')
+			setUpd2(true)
+		} else {
+			setUpd('rth')
+			setUpd2(false)
 		}
-		// console.log(e)
-		this.setState({ name: 'awd' })
+		console.log(contacts)
+		const inpNameElement = document.getElementById('inpName')
+		const inpPhoneElement = document.getElementById('inpPhone')
 
-		return this.state.contacts.push(cons)
+		if (inpNameElement && inpPhoneElement) {
+			const inpName = inpNameElement.value
+			const inpPhone = inpPhoneElement.value
+
+			let cons = {
+				id: nanoid(),
+				name: inpName,
+				number: inpPhone,
+			}
+
+			return contacts.push(cons)
+		} else {
+			console.log('Input elements not found')
+		}
 	}
 
-	renderContacts = (e) => {
+	const renderContacts = (e) => {
 		return e.map((info) => {
 			return (
 				<li className={sty.boxMap} key={nanoid()}>
@@ -36,109 +62,158 @@ class TypeInp extends Component {
 			)
 		})
 	}
-	handleFilterChange = (e) => {
-		this.setState((prevState) => ({
-			...prevState,
-			filter: e.target.value,
-		}))
-	}
-	componentDidMount() {
-		if (localStorage.getItem('contacts') == null || localStorage.getItem('contacts') == undefined) {
-			document.getElementById("list").textContent = "list"
+	const handleFilterChange = () => {
+		const inpFind = document.getElementById("inpFInd")
+		// setFilter(inpFind.value)
+		if (inpFind) {
+			const inpFindtw = inpFind.value
+
+
+			
+			return setFilter(inpFindtw)
 		} else {
-			document.getElementById("list").textContent = " "
-			let setInStorage = localStorage.getItem('contacts')
-		let parseinJson = JSON.parse(setInStorage)
-		let awdawd = this.state.contacts.push(...parseinJson)
-		// console.log(...parseinJson)
-		// console.log(this.state.contacts.push(...parseinJson))
-		console.log(this.state.contacts)
-		// setInterval(() => alert('tick'), 2000);
-		
-		// this.forceUpdate()
-		this.setState({ name: 'awd' })
-		// setTimeout(this.forceUpdate(), 1000);
+			console.log('Input elements not found')
 		}
 		
 	}
-	// JSON.stringify(this.state.contacts)
-	componentDidUpdate() {
-		let getLOcalConts = localStorage.getItem('contacts')
-		let setItems = JSON.parse(getLOcalConts)
-		console.log(this.state.contacts)
-		let setLocalContacts = localStorage.setItem(
-			'contacts',
-			JSON.stringify(this.state.contacts)
-		)
-		console.log(setItems)
+
+	const deleteItem = (e) => {
+		console.log(e)
 		
 	}
 
-	render() {
-		
-		
-		console.log('render')
-		const { filter, contacts } = this.state
-		// console.log(filter)
+
+	useEffect(() => {
+		if (isMounted === false) {
+			if (
+				localStorage.getItem('contacts') == null ||
+				localStorage.getItem('contacts') == undefined
+			) {
+			} else {
+				let setInStorage = localStorage.getItem('contacts')
+				let parseinJson = JSON.parse(setInStorage)
+				let awdawd = contacts.push(...parseinJson)
+				console.log(contacts)
+				setUpd('w')
+			}
+		}
+		if (isMounted) {
+			console.log('moundet')
+			console.log(isMounted)
+			let getLOcalConts = localStorage.getItem('contacts')
+			let setItems = JSON.parse(getLOcalConts)
+			console.log(contacts)
+			let setLocalContacts = localStorage.setItem(
+				'contacts',
+				JSON.stringify(contacts)
+			)
+			console.log(setItems)
+		}
+		setIsMounted(true)
+		// console.log('useEfeect')
 		// console.log(contacts)
-		// console.log(this.state)
+	})
 
-		return (
-			<div className={sty.box}>
-				<div className={sty.boxForm}>
-					<input
-						className={sty.inp}
-						type="text"
-						name="name"
-						pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-						title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-						required
-						id="inpName"
-						placeholder="name"
-					/>
+	// JSON.stringify(this.state.contacts)
+	// componentDidUpdate() {
+	// 	let getLOcalConts = localStorage.getItem('contacts')
+	// 	let setItems = JSON.parse(getLOcalConts)
+	// 	console.log(this.state.contacts)
+	// 	let setLocalContacts = localStorage.setItem(
+	// 		'contacts',
+	// 		JSON.stringify(this.state.contacts)
+	// 	)
+	// 	console.log(setItems)
 
-					<input
-						className={sty.inp}
-						placeholder="number"
-						type="tel"
-						name="number"
-						pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-						title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-						required
-						id="inpPhone"
-					/>
+	// })
+	// componentDidMount() {
+	// 	if (localStorage.getItem('contacts') == null || localStorage.getItem('contacts') == undefined) {
+	// 		document.getElementById("list").textContent = "list"
+	// 	} else {
+	// 		document.getElementById("list").textContent = " "
+	// 		let setInStorage = localStorage.getItem('contacts')
+	// 	let parseinJson = JSON.parse(setInStorage)
+	// 	let awdawd = this.state.contacts.push(...parseinJson)
+	// 	console.log(this.state.contacts)
 
-					<button onClick={() => this.addContact(this.state)}>SEND</button>
-				</div>
-				<h1>Contacts</h1>
+	// 	// this.forceUpdate()
+	// 	this.setState({ name: 'awd' })
+	// 	}
+
+	// }
+	// // JSON.stringify(this.state.contacts)
+	// componentDidUpdate() {
+	// 	let getLOcalConts = localStorage.getItem('contacts')
+	// 	let setItems = JSON.parse(getLOcalConts)
+	// 	console.log(this.state.contacts)
+	// 	let setLocalContacts = localStorage.setItem(
+	// 		'contacts',
+	// 		JSON.stringify(this.state.contacts)
+	// 	)
+	// 	console.log(setItems)
+
+	// }
+
+	console.log('render')
+	// const { filter, contacts } = this.state
+	// console.log(filter)
+	// console.log(contacts)
+	// console.log(this.state)
+
+	return (
+		<div className={sty.box}>
+			<div className={sty.boxForm}>
 				<input
 					className={sty.inp}
-					placeholder="find"
+					type="text"
+					name="name"
+					pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+					title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+					required
+					id="inpName"
+					placeholder="name"
+				/>
+
+				<input
+					className={sty.inp}
+					placeholder="number"
 					type="tel"
 					name="number"
 					pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
 					title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
 					required
-					id="inpFInd"
-					onChange={this.handleFilterChange}
+					id="inpPhone"
 				/>
 
-				<ul id='list'>
-					{this.state.contacts
-						.filter((contact) =>
-							contact.name
-								.toLowerCase()
-								.includes(this.state.filter.toLowerCase())
-						)
-						.map((contact) => (
-							<li key={contact.id}>
-								{contact.name}: {contact.number}
-							</li>
-						))}
-				</ul>
+				<button onClick={addContact}>SEND</button>
 			</div>
-		)
-	}
+			<h1>Contacts</h1>
+			<input
+				className={sty.inp}
+				placeholder="type fho find"
+				type="tel"
+				name="number"
+				pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+				title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+				required
+				id="inpFInd"
+				// onChange={handleFilterChange()}
+			/>
+<button onClick={handleFilterChange} id="btnFInd">find</button>
+			<ul id="list">
+				{contacts
+					.filter((contact) =>
+						contact.name.toLowerCase().includes(filter.toLowerCase())
+					)
+					.map((contact) => (
+						<li className={sty.item} key={contact.id}>
+							<p>{contact.name}: {contact.number}</p>
+							<button onClick={(e) => deleteItem(e.target)}>del</button>
+						</li>
+					))}
+			</ul>
+		</div>
+	)
 }
 
 export default TypeInp
